9320843324377b5ac4f8ecd381633629338a6bd1
Max <maxpolak97@gmail.com>
2023-10-05 16:22:54 +0200

    Update compare_runs.py

--- client/compare_runs.py
+++ client/compare_runs.py
@@ -1,7 +1,8 @@
 import os, wandb
 import wandb.apis.reports as wr
 
-assert os.getenv('WANDB_API_KEY'), 'You must set the WANDB_API_KEY environment variable'
+os.environ["RUN_ID"] = "th145a0f"
+# assert os.getenv('WANDB_API_KEY'), 'You must set the WANDB_API_KEY environment variable'
 
 def get_baseline_run(entity='max-deep', project='titanic_survived', tag='baseline'):
     "Get the baseline run from the project using tags"
